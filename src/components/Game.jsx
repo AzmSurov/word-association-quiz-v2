974f0a0e006465709bf15ef0963af165ec4578ba
AZM Mohibul Surov <azmsm1@AZMs-MacBook-Pro.local>
2022-08-24 16:58:28 -0500

    testing variables

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -219,7 +219,7 @@ if(score === 10) {
             </h1>
           </div>
           <div className="flex justify-center">
-            <button class="relative inline-flex py-2 items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800" onClick={() => setChosenLevel(null)}>
+            <button class="relative inline-flex py-2 items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white  focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800" onClick={() => setChosenLevel(null)}>
   <span class="relative px-5 py-2.5 transition-all ease-in duration-75  rounded-md group-hover:bg-opacity-0">
       Go Back
   </span>
